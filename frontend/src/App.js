959f8bb84dc5011985d37d171b6f656d2787c269
B V Vijay <bandikar.v.vijay@gmail.com>
2026-07-12 14:16:46 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,36 +5,14 @@ import {
 } from "react-router-dom";
 
 import CreateRoom from "./Pages/CreateRoom";
+
 import Room from "./Pages/Room";
 
 export default function App() {
 
-  // Change to false tomorrow
-  const maintenance = true;
-
-  if (maintenance) {
-    return (
-      <div
-        style={{
-          height: "100vh",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          flexDirection: "column",
-          background: "#0b1020",
-          color: "#fff",
-          fontFamily: "Arial",
-          textAlign: "center",
-          padding: "20px",
-        }}
-      >
-        <h1>🚧 No more SwipeMood </h1>
-      </div>
-    );
-  }
-
   return (
     <BrowserRouter>
+
       <Routes>
 
         <Route
@@ -48,6 +26,7 @@ export default function App() {
         />
 
       </Routes>
+
     </BrowserRouter>
   );
 }
